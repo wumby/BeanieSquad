3e50851990c245c7e57cdd66c3d60a27293676c9
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2025-02-02 22:54:44 -0700

    fix dumb select

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="w-full h-screen flex items-center flex-col overflow-x-hidden">
       <div className="absolute inset-0 bg-background-light dark:bg-background-dark bg-opacity-50"></div>
       {/* Content */}
-      <div className="flex justify-center flex-col w-fit h-[25vh] z-10">
+      <div className="flex justify-center flex-col w-fit h-[25vh] z-10 select-none">
         <h1 className="flex justify-center text-7xl">Beanie Squad</h1>
         <p className="mt-4 text-lg md:text-xl text-gray-300 w-fit">
           The ultimate 2K basketball squad. Compete. Dominate. Win.
